70b14cd7ca72a9d0fb43e9b11444b30b04e791f5
_LTD_ <xuppermario@gmail.com>
2020-03-31 21:40:04 +0700

    _LTD_

--- Python/13_LeTienDat_ex1.py
+++
@@ -1,9 +0,0 @@
-a = int(input("a: "))
-b = int(input("b: "))
-c = int(input("c: "))
-if a == b == c:
-    print("Equilateral triangle")
-elif a != b != c:
-    print("Scalene triangle")
-else:
-    print("Isosceles triangle")
