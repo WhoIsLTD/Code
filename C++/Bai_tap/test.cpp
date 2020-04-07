84b69dbb4726bc8fd5112061ceea93d7cd5f0e27
WhoIsLTD <xuppermario@gmail.com>
2020-04-07 22:14:46 +0700

    ltd

--- C++/Bai_tap/test.cpp
+++ C++/Bai_tap/test.cpp
@@ -31,3 +31,32 @@ ostream &operator<<(ostream &mh, phanso &p)
     mh << p.ts << "/" << p.ms;
     return mh;
 }
+istream &operator>>(istream &bp, phanso &p)
+{
+    cout << " Nhaptu so:";
+    bp >> p.ts;
+    cout << " Nhapmauso:";
+    bp >> p.ms;
+}
+template <class T>
+void swap(T &a, T &b)
+{
+    T tg;
+    tg = a;
+    a = b;
+    b = tg;
+}
+template <class T> void sort(T *a, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+        for (int j = i + 1; j < n; j++)
+            if (a[i] < a[j])
+                swap(a[i], a[j]);
+}
+template <class T>
+void print(T *a, int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << a[i] << "   ";
+    cout << endl;
+}
