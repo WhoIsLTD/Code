a45a2583dbfaf5ca3f4d3d037931cb7f3fbc3442
_LTD_ <xuppermario@gmail.com>
2020-04-08 11:07:47 +0700

    ltd

--- C++/Bai_tap/test.cpp
+++ C++/Bai_tap/test.cpp
@@ -31,3 +31,62 @@ ostream &operator<<(ostream &mh, phanso &p)
     mh << p.ts << "/" << p.ms;
     return mh;
 }
+istream &operator>>(istream &bp, phanso &p)
+{
+    cout << "Nhap tu so: ";
+    bp >> p.ts;
+    cout << "Nhap mau so: ";
+    bp >> p.ms;
+    return bp;
+}
+template <class T>
+void Swap(T &a, T &b)
+{
+    T tg;
+    tg = a;
+    a = b;
+    b = tg;
+}
+template <class T>
+void sort(T *a, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+        for (int j = i + 1; j < n; j++)
+            if (a[i] < a[j])
+                Swap(a[i], a[j]);
+}
+template <class T>
+void print(T *a, int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << a[i] << "   ";
+    cout << endl;
+}
+int main()
+{
+    int a[10], n;
+    phanso b[10];
+    cout << "Nhap n " << endl;
+    cin >> n;
+    cout << "Nhap cac phan tu cho mang a: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "a[" << (i + 1) << "]=";
+        cin >> a[i];
+    }
+    cout << "Nhap cac phan tu cho mang b: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> b[i];
+    }
+    cout << "Cac phan tu cua mang a truoc khi sap xep la:" << endl;
+    print(a, n);
+    sort(a, n);
+    cout << "Cac phan tu cua mang a sau khi sap xep la: " << endl;
+    print(a, n);
+    cout << "Cac phan tu cua mang b truoc khi sap xep la: " << endl;
+    print(b, n);
+    sort(b, n);
+    cout << "Cac phan tu cua mang b sau khi sap xep la: " << endl;
+    print(b, n);
+}
