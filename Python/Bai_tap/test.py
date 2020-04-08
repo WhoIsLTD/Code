0135863c08c275e2de96cd3ea7034e5e4dd4319f
_LTD_ <xuppermario@gmail.com>
2020-04-08 10:33:23 +0700

    _LTD_

--- Python/Bai_tap/test.py
+++ Python/Bai_tap/test.py
@@ -1,14 +1,1 @@
-m = int(input())
-n = int(input())
-dem = 0
-if m < n:
-    for i in range(m, n+1):
-        if i % 3 == 0 or i % 5 == 0:
-            dem += 1
-            print(i)
-    print(dem)
-for i in range(m, n-1, -1):
-    if i % 3 == 0 or i % 5 == 0:
-        dem += 1
-        print(i)
-print(dem)
+
