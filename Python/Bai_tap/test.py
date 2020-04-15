403b85d126f2a2100572471780ffabe1a410dbed
_LTD_ <xuppermario@gmail.com>
2020-04-15 10:24:29 +0700

    ltd

--- Python/Bai_tap/test.py
+++ Python/Bai_tap/test.py
@@ -7,11 +7,16 @@ a = {
     'E' : ['F'],
     'F' : []
 }
+
 s = []
-def dfs(a, s, key):
-    if key not in s:
+s1 = []
+def dfs(a, s, key, key2):
+    if(a[key] != [] or key =='F'):
         s.append(key)
         print(s)
+    
     for i in a[key]:
-        dfs(a, s, i)
-dfs(a, s, "A")
+        if 'F' in s:
+            break
+        dfs(a, s, i, key2)
+dfs(a, s, 'A', 'F')
