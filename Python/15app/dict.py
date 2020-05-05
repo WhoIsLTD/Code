6b78fd960fdee1804441a0cb1b7b8af024930dbb
WhoIsLTD <xuppermario@gmail.com>
2020-05-05 22:10:43 +0700

    ltd

--- Python/15app/dict.py
+++ Python/15app/dict.py
@@ -3,11 +3,14 @@ import json
 data = json.load(open("/media/E:/Code/Python/15app/original.json"))
 
 def translate(word):
+    word = word.lower()
     if word in data:
         return data[word]
+    elif word.title() in data:
+        return data[word.title()]
     else:
         print("Pugger your paw steps on wrong keys")
     
-word = input("Enter the word you want to search")
+word = input("Enter the word you want to search: ")
 output = translate(word)
 print(output)
