cf7d0a42bb5f9b04f33f7b2894a6c5565bd88907
_LTD_ <41822445+WhoIsLTD@users.noreply.github.com>
2020-07-15 22:48:20 +0700

    hello

---
+++ C#/ReadAndLitsen/ReadAndLitsen/ListItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadAndLitsen
+{
+    public class ListItem
+    {
+        public ListItem()
+        {
+            item = new List<Item>();
+        }
+        public List<Item> item { get; set; }
+    }
+}
