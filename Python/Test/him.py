e15e5e5661166fd99a0431eef0b9916ad7cdd6d4
_LTD_ <41822445+WhoIsLTD@users.noreply.github.com>
2020-08-29 10:12:54 +0700

    commit

--- Python/Test/him.py
+++ Python/Test/him.py
@@ -1,18 +1,5 @@
-from pytube import Youtube
-
-#ask for the link from user
-link = input("Enter the link of YouTube video you want to download:  ")
-yt = YouTube(link)
-
-#Showing details
-print("Title: ",yt.title)
-print("Number of views: ",yt.views)
-print("Length of video: ",yt.length)
-print("Rating of video: ",yt.rating)
-#Getting the highest resolution possible
-ys = yt.streams.get_highest_resolution()
-
-#Starting download
-print("Downloading...")
-ys.download()
-print("Download completed!!")
+sentence = input()
+end = sentence[-1]
+sentence = sentence[:-1]
+liste = sentence.split()[::-1]
+print(liste)
